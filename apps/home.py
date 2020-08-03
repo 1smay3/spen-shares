f69fc169702f7b850c5f08e1e4ed815db601937b
1smay3 <spencermayy@gmail.com>
2020-08-03 17:16:03 +0100

    adding files

---
+++ apps/home.py
@@ -0,0 +1,76 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from index import navbar
+# needed only if running this as a single page app
+#external_stylesheets = [dbc.themes.LUX]
+
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#https://dash-bootstrap-components.opensource.faculty.ai/docs/components/layout/
+# change to app.layout if running as single page app instead
+
+DCF_card_content = dbc.Card(
+    [
+        dbc.CardImg(src="https://image.flaticon.com/icons/svg/2172/2172450.svg", top=True),
+        dbc.CardBody(
+            [
+                html.H4("DCF Estimation", className="card-title"),
+                html.P(
+                    "Some quick example text to build on the card title and "
+                    "make up the bulk of the card's content.",
+                    className="card-text",
+                ),
+            dbc.Button("DCF", color="primary"),
+            ]
+        ),
+    ],
+style={"height": "18rem", "width":"18rem"},
+)
+git add .
+PE_card_content = [
+    dbc.CardHeader("P/E Ratio"),
+    dbc.CardBody(
+        [
+            dbc.CardImg(src="https://image.flaticon.com/icons/svg/2172/2172450.svg", top=True),
+            html.P(
+                "Historical P/E ratio for all S&P500 companies",
+                className="card-text",
+            ),
+        ]
+    ),
+]
+
+PB_card_content = [
+    dbc.CardHeader("P/B Ratio"),
+    dbc.CardBody(
+        [
+            dbc.CardImg(src="https://image.flaticon.com/icons/svg/2172/2172450.svg", top=True),
+            html.P(
+                "Historical P/B ratio for all S&P500 companies",
+                className="card-text",
+            ),
+        ]
+    ),
+]
+
+
+padding = html.Div(
+    [
+        dbc.Row(dbc.Col(html.Div("")))])
+
+cards =dbc.Row(
+            [
+                dbc.Col(dbc.Card(DCF_card_content, color="info", inverse=False), width="2"),
+                dbc.Col(dbc.Card(DCF_card_content, inverse=True), width="2"),
+                dbc.Col(dbc.Card(DCF_card_content, inverse=True), width="2"),
+            ],
+            className="mb-5",justify="center"
+        )
+
+layout = html.Div([navbar,padding,cards])
+
+
+
+
+
+
+
---
+++ index.py
@@ -0,0 +1,67 @@
+import dash, dash_renderer
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+from apps import home
+
+BS = "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
+
+#Simple Navbar cant have logo. Leave that for now.
+
+
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Home", href="/Home"), id="Home-link"),
+        dbc.NavItem(dbc.NavLink("DCF", href="/DCF"), id="DCF-link"),
+        dbc.NavItem(dbc.NavLink("PE", href="/PE"), id="PE-link"),
+        dbc.NavItem(dbc.NavLink("PB", href="/PB"), id="PB-link")
+
+    ],
+    brand="SpenShares",
+    brand_href="/",
+    color="primary",
+    dark=True,
+)
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+
+dcf_page_layout = html.Div([navbar, html.H1('DCF')])
+PE_page_layout = html.Div([navbar, html.H1('PE')])
+PB_page_layout = html.Div([navbar, html.H1('PB')])
+
+
+# Update the index
+@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/DCF':
+        return dcf_page_layout
+    elif pathname == '/PE':
+        return PE_page_layout
+    elif pathname == '/PB':
+        return PB_page_layout
+    else:
+        return home.layout
+        # You could also return a 404 "URL not found" page here
+
+
+# Update which link is active in the navbar
+# If there's lots of pages, we can probably be cleverer about generating
+# these functions dynamically.
+#@app.callback(Output('page-1-link', 'active'), [Input('url', 'pathname')])
+#def set_page_1_active(pathname):
+    #return pathname == '/page-1'
+
+#@app.callback(Output('page-2-link', 'active'), [Input('url', 'pathname')])
+#def set_page_2_active(pathname):
+    #return pathname == '/page-2'
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8888)
