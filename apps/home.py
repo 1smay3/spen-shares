b762b6be0a50bbf0a4bedcd82043ed6ab37761a1
1smay3 <spencermayy@gmail.com>
2020-08-03 21:56:28 +0100

    Added button hyperlink

--- apps/home.py
+++ apps/home.py
@@ -22,7 +22,7 @@ DCF_card_content = dbc.Card(
                     "make up the bulk of the card's content.",
                     className="card-text",
                 ),
-            dbc.Button("DCF", color="primary"),
+            dbc.Button("DCF", color="primary",href="/DCF"),
             ]
         ),
     ],
@@ -37,7 +37,7 @@ PE_card_content = [
                 "Historical P/E ratio for all S&P500 companies",
                 className="card-text",
             ),
-            dbc.Button("P/E Ratio", color="primary")
+            dbc.Button("P/E Ratio", color="primary",href="/PE")
         ]
     ),
 ]
@@ -51,7 +51,7 @@ PB_card_content = [
                 "Historical P/B ratio for all S&P500 companies",
                 className="card-text",
             ),
-            dbc.Button("P/B Ratio", color="primary")
+            dbc.Button("P/B Ratio", color="primary",href="/PB")
         ]
     ),
 ]
