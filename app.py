73915a5c825cff45c045f135f3a99c63dcb9ec84
1smay3 <spencermayy@gmail.com>
2020-08-07 00:00:32 +0100

    Added favicon and app title for browser tab information

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ import dash_bootstrap_components as dbc
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+app.title = 'SpenShares'
 server = app.server
 app.config.suppress_callback_exceptions = True
